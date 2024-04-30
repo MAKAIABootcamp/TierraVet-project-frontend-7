3fac4589c35eb350ea8e0439a66ff304631be510
ibelabrador <ibelabrador@gmail.com>
2024-04-30 11:56:51 -0500

    def colores

--- tierravet/tailwind.config.js
+++ tierravet/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+       colors: {
+        primary: '#E1FAEF',
+        secondary: '#AFCFBF',
+        tertiary: '#C0C0C0'
+      },
+    },
   },
   plugins: [],
 }
+
