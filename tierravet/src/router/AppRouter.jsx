93cb648f7edc0759f21852ea54b130d81d92e2ab
ibelabrador <ibelabrador@gmail.com>
2024-05-01 19:14:14 -0500

    vista procedure

--- tierravet/src/router/AppRouter.jsx
+++ tierravet/src/router/AppRouter.jsx
@@ -1,12 +1,14 @@
 // AppRouter.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home'; 
+import Procedure from '../Pages/Procedure';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path='procedure' element={<Procedure/>}/>
       </Routes>
     </Router>
   );
