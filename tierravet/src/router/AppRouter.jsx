4e904e583c64447dcce451ee9e9c360b4ce8e28a
ibelabrador <ibelabrador@gmail.com>
2024-05-01 20:01:28 -0500

    vista vaccination

--- tierravet/src/router/AppRouter.jsx
+++ tierravet/src/router/AppRouter.jsx
@@ -1,12 +1,14 @@
 // AppRouter.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home'; 
+import Vaccination from '../Pages/Vaccination';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path='vaccination' element={<Vaccination/>}/>
       </Routes>
     </Router>
   );
