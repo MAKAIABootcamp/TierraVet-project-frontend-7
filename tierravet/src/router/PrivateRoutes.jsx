48a52f0daa2ca4a9b5a416bf71677329eca5fb36
Angelz04 <angelosbh2004@gmail.com>
2024-05-21 17:44:46 -0500

    ajuste ruta home

--- tierravet/src/router/PrivateRoutes.jsx
+++ tierravet/src/router/PrivateRoutes.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
-const PrivateRoutes = ({ redirectPath = "/login" }) => {
+const PrivateRoutes = ({ redirectPath = "/home" }) => {
   const { isAuth } = useSelector((store) => store.auth);
   if (!isAuth) return <Navigate to={redirectPath} />;
   return <Outlet />;
