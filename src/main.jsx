b8ab44c42e0fc459b493de3e0206516f52bf7bb7
Angelz04 <angelosbh2004@gmail.com>
2024-05-22 23:29:42 -0500

    deploy gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import ReactDOM from "react-dom/client";
 import AppRouter from "./router/AppRouter";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <AppRouter />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
